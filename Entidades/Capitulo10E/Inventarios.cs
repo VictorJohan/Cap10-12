5872437a9cf46a383d07ef6f7aa22e7a3bcaa2e8
Victor Johan <Vicjohan23@gmail.com>
2020-06-18 23:36:59 -0400

    SE CREO LA CLASE Inventario EN LA CARPETA Entidades\Capitulo10E.

---
+++ Entidades/Capitulo10E/Inventarios.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cap10_12.Entidades.Capitulo10E
+{
+   public class Inventarios
+   {
+        public string NombreArticulo { get; set; }
+        public int IdArticulo { get; set; }
+        public int CantidadArticulo { get; set; }
+        public double PrecioArticulo { get; set; }
+   }
+}
