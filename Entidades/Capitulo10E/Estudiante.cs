bbffdc9d7de925ef457bcf7a50a85f1eaf539e97
Victor Johan <Vicjohan23@gmail.com>
2020-06-18 23:51:13 -0400

    SE CREO LA CLASE Estudiantes EN LA CARPETA Entidades\Capitulo10E

---
+++ Entidades/Capitulo10E/Estudiante.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cap10_12.Entidades.Capitulo10E
+{
+   public class Estudiante
+   {
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string Matricula { get; set; }
+        public string Curso { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public DateTime FechaIngreso { get; set; } = DateTime.Now;
+        public DateTime FechaEgreso { get; set; }
+        public float Calificacion { get; set; }
+        public ArrayList RecordCalificaciones { get; set; }
+        public string NombreMadre { get; set; }
+        public string NombrePadre { get; set; }
+        public string NombreTutor { get; set; }
+   }
+}
